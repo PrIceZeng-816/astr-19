895d1ff19d29f87ef5f7777c7520e4e715069152
PrIceZeng-816 <lzeng13@ucsc.edu>
2025-09-26 11:06:43 -0700

    part 2

---
+++ basic_math.py
@@ -0,0 +1,22 @@
+class calculator:
+    def addition(self,x,y):
+        return x+y
+    def subtraction(self,x,y):
+        return x-y
+    def multiplication(self,x,y):
+        return x*y
+
+test = calculator()
+
+# sum of two floating point numbers
+result = test.addition(1.2,2.1)
+print(result, type(result))
+# difference between two integers
+result = test.subtraction(10,20)
+print(result, type(result))
+#product of a floating point number and an integer
+result = test.multiplication(2.2,10)
+print(result, type(result))
+
+
+
