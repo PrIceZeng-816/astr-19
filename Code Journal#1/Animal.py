b8794e087ee73519c9ae2ae6985b2c4bd2609ac0
PrIceZeng-816 <lzeng13@ucsc.edu>
2025-09-30 12:25:47 -0700

    Write a Python program that declares a class describing your favorite animal. Have the data members of the class represent the following physical parameters of the animal: length of the arms (float), length of the legs (float), number of eyes (int), does it have a tail? (bool), is it furry? (bool). Write an initialization function that sets the values of the data members when an instance of the class is created. Write a member function of the class to print out and describe the data members representing the physical characteristics of the animal.

--- Code Journal#1/Animal.py
+++ Code Journal#1/Animal.py
@@ -1,3 +1,12 @@
+"""
+Write a Python program that declares a class describing your favorite animal.
+Have the data members of the class represent the following physical parameters of the animal:
+length of the arms (float), length of the legs (float), number of eyes (int), does it have a tail? (bool),
+is it furry? (bool). Write an initialization function that sets the values of the data members when an instance of
+the class is created. Write a member function of the class to print out and describe the data members representing the
+physical characteristics of the animal.
+"""
+
 class cat():
     def __init__(self, arm_length, leg_length, eyes_num, tail, furry):
         self.arm_length = arm_length
