abd91883d11e6fe332ef86ea57049d19c711d036
PrIceZeng-816 <lzeng13@ucsc.edu>
2025-09-30 12:23:48 -0700

    Write a Python that prints the sum of two floating point numbers, the difference between two integers, and the product of a floating point number and an integer. In each case, have the program print out the data type of the resulting answer.

--- Code Journal#1/basic_math.py
+++ Code Journal#1/basic_math.py
@@ -1,3 +1,9 @@
+"""
+Write a Python that prints the sum of two floating point numbers,
+the difference between two integers, and
+the product of a floating point number and an integer.
+In each case, have the program print out the data type of the resulting answer.
+"""
 # git push straight from pycharm
 class calculator:
     def addition(self,x,y):
