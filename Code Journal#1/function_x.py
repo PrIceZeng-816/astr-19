0cb437a710b36d4deb99ed56e3003ba837c51a67
PrIceZeng-816 <lzeng13@ucsc.edu>
2025-09-30 12:24:33 -0700

    Write a Python program that defines a function f(x) that returns x**3 + 8. In the main function of the program, call f(x) with x = 9 and print the result.  Use an if statement that executes if the result is larger than 27 and prints “YAY!”.

--- Code Journal#1/function_x.py
+++ Code Journal#1/function_x.py
@@ -1,3 +1,9 @@
+"""
+Write a Python program that defines a function f(x) that returns x**3 + 8.
+In the main function of the program, call f(x) with x = 9 and print the result.
+Use an if statement that executes if the result is larger than 27 and prints “YAY!”.
+"""
+
 def f(a):
     return a**3 + 8
 
