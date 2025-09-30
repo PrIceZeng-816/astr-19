a5f1eb537d7730e1ced522231a1a68d533ed65fd
PrIceZeng-816 <lzeng13@ucsc.edu>
2025-09-30 12:26:31 -0700

    Write a Python program that writes out a table of the function sin(x) vs. x, where x is tabulated between 0 and 2 with a thousand entries. Follow the basic Python program structure, including a main program function.

--- Code Journal#1/table_sin(x).py
+++ Code Journal#1/table_sin(x).py
@@ -1,3 +1,8 @@
+"""
+Write a Python program that writes out a table of the function sin(x) vs. x,
+where x is tabulated between 0 and 2 with a thousand entries. Follow the basic Python program structure,
+including a main program function.
+"""
 import math
 from tabulate import tabulate
 from matplotlib import pyplot
